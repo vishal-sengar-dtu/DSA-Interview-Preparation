05f9fc2eb06712a921e77a2a7678d5cefc0cf9cc
Vishal Sengar <vishalsengardtu@gmail.com>
2021-12-14 15:40:04 +0530

    Maximum Sum Subarray

--- Arrays/kadane.cpp
+++ Arrays/kadane.cpp
@@ -4,8 +4,21 @@ using namespace std;
 class Kadane
 {
 public:
-    int maxSumSubarray(int arr[], int n){
-        
+    int maxSumSubarray(int arr[], int n)
+    {
+        int maxSum = 0, sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+
+            if (maxSum < sum)
+                maxSum = sum;
+                
+            if (sum < 0)
+                sum = 0;
+        }
+        return maxSum;
     };
 
     int maxProductSubarray(int arr[], int n)
