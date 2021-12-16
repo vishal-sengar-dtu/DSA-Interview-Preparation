34da79c900fd58ee131c21ffdcde6e0888013393
Vishal Sengar <vishalsengardtu@gmail.com>
2021-12-16 16:29:46 +0530

    Reverse Letter of Words in a String

--- Strings/reverseWords.cpp
+++ Strings/reverseWords.cpp
@@ -4,6 +4,9 @@ using namespace std;
 class Solution
 {
 public:
+    // input: str = " the sky   is blue  "
+    // output: "blue is the sky"
+
     string reverseWords(string str) // Time O(N), Space O(N)
     {
         stack<string> st;
@@ -12,18 +15,14 @@ public:
         for (int i = 0; i < str.length(); i++)
         {
             if (str[i] != ' ')
-            {
                 temp += str[i];
-            }
 
             if (temp.length() != 0)
-            {
                 if (str[i] == ' ' || i == str.length() - 1)
                 {
                     st.push(temp);
                     temp = "";
                 }
-            }
         }
 
         string res = "";
@@ -36,5 +35,12 @@ public:
         }
 
         return res;
+    }
+
+    // METHOD-2 (In-Place)
+    // reverse individual words ignoring extra spaces, and then reverse the whole string.
+
+    string reverseWords(string str) // Time O(N), Space O(1)
+    {
     }
 };
---
+++ Strings/reverseWordsII.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // input: str = "the sky is blue"
+    // output: "eht yks si eulb"
+
+    string reverseWords(string str) // Time O(N), Space O(1)
+    {
+        int first = 0, last = 0;
+
+        while (last < str.length())
+        {
+            if (str[last] == ' ')
+            {
+                reverse(str.begin() + first, str.begin() + last);
+                first = last + 1;
+            }
+            if (last == str.length() - 1)
+                reverse(str.begin() + first, str.end());
+
+            last++;
+        }
+        return str;
+    }
+};
