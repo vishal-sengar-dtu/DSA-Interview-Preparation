1f0b821220f85d71b79038936b23cbf80c6fd9c0
Vishal Sengar <vishalsengardtu@gmail.com>
2021-12-16 14:50:55 +0530

    Integer to Roman

---
+++ Arrays & Strings/integerToRoman.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    vector<int> integer = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    vector<string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+public:
+    string intToRoman(int num)
+    {
+        string ans = "";
+
+        for (int i = 0; i < integer.size(); i++)
+        {
+            if (num >= integer[i])
+            {
+                ans += roman[i];
+                num -= integer[i--];
+            }
+        }
+        return ans;
+    }
+};
