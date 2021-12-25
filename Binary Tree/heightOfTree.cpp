b207cd2556297185ab4a57d3c45d05fec9ebc2f0
Vishal Sengar <vishalsengardtu@gmail.com>
2021-12-25 12:20:45 +0530

    Height of a Tree

---
+++ Binary Tree/heightOfTree.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left, *right;
+};
+
+class Solution // (https://leetcode.com/problems/maximum-depth-of-binary-tree/)
+{
+public:
+    // height = -1 ; for empty tree.
+
+    int height(TreeNode *root)
+    {
+        if (root == NULL)
+            return -1;
+
+        return 1 + max(height(root->left), height(root->right));
+    }
+};
--- Binary Tree/levelOrderTraversal.cpp
+++ Binary Tree/levelOrderTraversal.cpp
@@ -8,10 +8,26 @@ public:
     TreeNode *left, *right;
 };
 
-class Solution
+class Solution // (https://leetcode.com/problems/binary-tree-level-order-traversal/)
 {
+    vector<vector<int>> lvlOrder;
+
+    void helper(TreeNode *root, int level)
+    {
+        if (root == NULL)
+            return;
+
+        if (lvlOrder.size() == level)
+            lvlOrder.push_back(vector<int>());
+
+        lvlOrder[level].push_back(root->val);
+
+        helper(root->left, level + 1);
+        helper(root->right, level + 1);
+    }
+
 public:
-    vector<vector<int>> levelOrder(TreeNode *root)
+    vector<vector<int>> levelOrder(TreeNode *root) // Iterative Method
     {
         vector<vector<int>> ans;
 
@@ -40,5 +56,11 @@ public:
             ans.push_back(curr_Lvl);
         }
         return ans;
+    }
+
+    vector<vector<int>> levelOrder(TreeNode *root) // Recursive Method
+    {
+        helper(root, 0);
+        return lvlOrder;
     }
 };
