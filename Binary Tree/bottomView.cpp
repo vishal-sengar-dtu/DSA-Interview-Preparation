74d250022190917f91fe1d1461095166df2f541c
Vishal Sengar <vishalsengardtu@gmail.com>
2021-12-27 14:41:19 +0530

    Bottom View of Binary Tree

---
+++ Binary Tree/bottomView.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left, *right;
+};
+
+class Solution // (https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1)
+{
+    void traverse(TreeNode *root, map<int, vector<int>> &mp, int x)
+    {
+        if (root)
+        {
+            mp[x].push_back(root->val);
+            traverse(root->left, mp, x - 1);
+            traverse(root->right, mp, x + 1);
+        }
+        return;
+    }
+
+public:
+    vector<int> topView(TreeNode *root) // Recursive Method
+    {
+        // mp = < x-coordinate, < root->val, . . . >  >
+        map<int, vector<int>> mp;
+        traverse(root, mp, 0);
+
+        vector<int> ans;
+
+        for (auto p : mp)
+        {
+            int sz = p.second.size();
+            ans.push_back(p.second[sz - 1]);
+        }
+        return ans;
+    }
+
+    vector<int> topView(TreeNode *root) // Iterative Method
+    {
+        queue<pair<TreeNode *, int>> Q;
+        map<int, vector<int>> mp;
+
+        Q.push({root, 0});
+
+        while (!Q.empty())
+        {
+            pair<TreeNode *, int> tmp = Q.front();
+            TreeNode *node = tmp.first;
+            int x = tmp.second;
+
+            Q.pop();
+            mp[x].push_back(node->val);
+
+            if (node->left)
+                Q.push({node->left, x - 1});
+            if (node->right)
+                Q.push({node->right, x + 1});
+        }
+
+        vector<int> ans;
+
+        for (auto &p : mp)
+        {
+            int sz = p.second.size();
+            ans.push_back(p.second[sz - 1]);
+        }
+
+        return ans;
+    }
+};
