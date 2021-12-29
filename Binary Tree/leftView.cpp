027d7058d906094527b132bae6635ad56e936375
Vishal Sengar <vishalsengardtu@gmail.com>
2021-12-29 09:13:01 +0530

    Left View of Binary Tree

---
+++ Binary Tree/leftView.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left, *right;
+};
+
+class Solution // (https://leetcode.com/problems/binary-tree-right-side-view/)
+{
+public:
+    vector<int> leftSideView(TreeNode *root) // Iterative Method
+    {
+        queue<TreeNode *> Q;
+        vector<int> ans;
+        
+        if (root == NULL)
+            return ans;
+
+        Q.push(root);
+
+        while (!Q.empty())
+        {
+            int sz = Q.size();
+
+            for (int i = 0; i < sz; i++)
+            {
+                TreeNode *tmp = Q.front();
+                Q.pop();
+
+                if (tmp->left)
+                    Q.push(tmp->left);
+                if (tmp->right)
+                    Q.push(tmp->right);
+                if (i == 0)
+                {
+                    ans.push_back(tmp->val);
+                }
+            }
+        }
+        return ans;
+    }
+};
