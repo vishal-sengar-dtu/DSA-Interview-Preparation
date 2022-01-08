affd6da130f7c74b1b28fe07cc85ece452e3ca08
Vishal Sengar <vishalsengardtu@gmail.com>
2022-01-09 01:24:28 +0530

    k Distance From Root

--- Binary Tree/kLevelsDown.cpp
+++ Binary Tree/kLevelsDown.cpp
@@ -3,11 +3,34 @@ using namespace std;
 
 class TreeNode
 {
+public:
     int val;
     TreeNode *left, *right;
 };
 
-class Solution // ()
+class Solution // (https://practice.geeksforgeeks.org/problems/k-distance-from-root/1/)
 {
+    void traverse(TreeNode *root, int k, vector<int> kLevelNodes)
+    {
+        if (root)
+        {
+            if (k == 0)
+            {
+                kLevelNodes.push_back(root->val);
+                return;
+            }
+
+            traverse(root->left, k - 1, kLevelNodes);
+            traverse(root->right, k - 1, kLevelNodes);
+        }
+        return;
+    }
+
 public:
+    vector<int> Kdistance(TreeNode *root, int k)
+    {
+        vector<int> kLevelNodes;
+        traverse(root, k, kLevelNodes);
+        return kLevelNodes;
+    }
 };
