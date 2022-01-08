3f72f380166f2993d9ceb6d3f2cdb144e7331cb1
Vishal Sengar <vishalsengardtu@gmail.com>
2022-01-09 00:10:55 +0530

    Node to Root Path

--- Binary Tree/rootToNodePath.cpp
+++ Binary Tree/rootToNodePath.cpp
@@ -3,15 +3,42 @@ using namespace std;
 
 class TreeNode
 {
+public:
     int val;
     TreeNode *left, *right;
 };
 
 class Solution // (https://www.interviewbit.com/problems/path-to-given-node/)
 {
+    bool findNode(TreeNode *root, int num, vector<int> &path)
+    {
+        if (root)
+        {
+            if (root->val == num)
+            {
+                path.push_back(root->val);
+                return true;
+            }
+            if (findNode(root->left, num, path))
+            {
+                path.push_back(root->val);
+                return true;
+            }
+            if (findNode(root->right, num, path))
+            {
+                path.push_back(root->val);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
 public:
     vector<int> nodePath(TreeNode *root, int num)
     {
-        
+        vector<int> path;
+        findNode(root, num, path);
+        return path;
     }
 };
---
+++ Linked List/reverseBetween.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+};
+
+class Solution // (https://leetcode.com/problems/reverse-linked-list-ii/)
+{
+public:
+    ListNode *reverseBetween(ListNode *head, int left, int right)
+    {
+        
+    }
+};
