97c4ff2bcb3e61f32276d2e0df410d4912fdb125
Vishal Sengar <vishalsengardtu@gmail.com>
2022-02-04 15:42:49 +0530

    3 Sum

--- Arrays/3Sum.cpp
+++ Arrays/3Sum.cpp
@@ -11,6 +11,34 @@ class Solution
 public:
     vector<vector<int>> threeSum(vector<int> &nums)
     {
-        
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if ((i > 0) && (nums[i] == nums[i - 1]))
+                continue;
+
+            int l = i + 1, r = nums.size() - 1;
+            while (l < r)
+            {
+                int s = nums[i] + nums[l] + nums[r];
+                if (s > 0)
+                    r--;
+                else if (s < 0)
+                    l++;
+                else
+                {
+                    res.push_back(vector<int>{nums[i], nums[l++], nums[r--]});
+                    
+                    while ((l < r) && (nums[l] == nums[l - 1] && nums[r] == nums[r + 1]))
+                    {
+                        l++;
+                        r--;
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
